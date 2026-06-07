a16dfce1a1b3466f4212a6b26f69767bc6a9200b
cvlnpsg987 <cvlnpsg987@gmail.com>
2026-06-07 06:21:03 +0000

    text fields added for signup page

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/features/auth/presentation/widgets/auth_field.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -11,17 +12,23 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Sign Up.",
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight:  FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Sign Up.",
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
             ),
-          )
-        ],
+            const SizedBox(height: 30),
+            AuthField(hintText: "Name"),
+            const SizedBox(height: 15),
+            AuthField(hintText: "Email"),
+            const SizedBox(height: 15),
+            AuthField(hintText: "Password"),
+          ],
+        ),
       ),
     );
   }
