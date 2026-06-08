180d3f5e7dbcf9127678676fbcd971c0cbe7d2a6
cvlnpsg987 <cvlnpsg987@gmail.com>
2026-06-08 04:35:08 +0000

    sign up pag UI done

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/core/theme/app_pallete.dart';
 import 'package:myapp/features/auth/presentation/widgets/auth_field.dart';
+import 'package:myapp/features/auth/presentation/widgets/auth_gradient_button.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -27,6 +29,24 @@ class _SignupPageState extends State<SignupPage> {
             AuthField(hintText: "Email"),
             const SizedBox(height: 15),
             AuthField(hintText: "Password"),
+            const SizedBox(height: 20),
+            AuthGradientButton(),
+            const SizedBox(height: 20),
+            RichText(
+              text: TextSpan(
+                text: "Don't have an account? ",
+                style: Theme.of(context).textTheme.titleMedium,
+                children: [
+                  TextSpan(
+                    text: "Sign In",
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: AppPallete.gradient2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
